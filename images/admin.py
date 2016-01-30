0b5805f11995c202b340d81474396b18b4d9eea2
Dmytro Domin <mr.diomin@gmail.com>
2016-01-31 00:51:16 +0200

    Image model registered to admin panel

--- images/admin.py
+++ images/admin.py
@@ -7,3 +7,6 @@ from .models import Image
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'image', 'created']
     list_filter = ['created']
+
+admin.site.register(Image, ImageAdmin)
+
