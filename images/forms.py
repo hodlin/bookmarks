02683ecd36d1bb04fa24514ccfd15d47e7cb19f7
Dmytro Domin <mr.diomin@gmail.com>
2016-01-31 01:13:45 +0200

    Image creation form URL validation method added

--- images/forms.py
+++ images/forms.py
@@ -9,3 +9,13 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,
         }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not'\
+                                        'match valid image extensions.')
+        return url
+
