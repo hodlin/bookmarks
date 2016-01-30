b56c15cdd243b1b499d6ddfc5b7b79a25c777845
Dmytro Domin <mr.diomin@gmail.com>
2016-01-31 01:28:14 +0200

    Image creation form save method overwritten

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,7 @@
+from urllib import request
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from .models import Image
 
 
@@ -19,3 +22,18 @@ class ImageCreateForm(forms.ModelForm):
                                         'match valid image extensions.')
         return url
 
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super(ImageCreateForm, self).save(commit=False)
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title),
+                                    image_url.rsplit('.', 1)[1].lower())
+
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.read()),
+                         save=False)
+
+        if commit:
+            image.save()
+        return image
