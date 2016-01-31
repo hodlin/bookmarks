2180076d154c8a83a5ef68a054795fa8f687b91d
Dmytro Domin <mr.diomin@gmail.com>
2016-01-31 17:29:25 +0200

    detail view for Images

--- images/views.py
+++ images/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from .models import Image
 
 # Create your views here.
 
@@ -29,3 +30,9 @@ def image_create(request):
 
     return render(request, 'images/image/create.html', {'section': 'images',
                                                         'form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request, 'images/image/detail.html', {'section': 'images',
+                                                        'image': image})
