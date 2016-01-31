2e03113bc2afafecc086e232db13436c6612fc17
Dmytro Domin <mr.diomin@gmail.com>
2016-01-31 11:03:32 +0200

    URLS to 'imges' app specified

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -21,6 +21,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('account.urls')),
+    url(r'^images/', include('images.urls', namespace='images')),
     url(r'social-auth/', include('social.apps.django_app.urls', namespace='social')),
 ]
 
--- images/views.py
+++ images/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib. import messages
+from django.contrib import messages
 from .forms import ImageCreateForm
 
 # Create your views here.
