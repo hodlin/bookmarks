2b10fd0a23f383e4bfae117837ae94798c903244
Dmytro Domin <mr.diomin@gmail.com>
2016-01-31 14:37:42 +0200

    bokmark an image on user's click

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -38,6 +38,19 @@
                 jQuery('#bookmarklet .images').append('<a href="#"><img src="' + image_url + '" /></a>');
             }
         });
+
+        // When an image is selected open URL with it
+        jQuery('#bookmarklet .image a').click(function(e) {
+            selected_image = jQuery(this).children('img').attr('src');
+            // hide bookmarklet
+            jQuery('#bookmarklet').hide();
+            // open new window to submit the image
+            window.open(site_url + 'images/create/?url='
+                        + encodeURIComponent(selected_image)
+                        + '&title='
+                        + encodeURIComponent(jQuery('title').text()),
+                        '_blank');
+        });
     };
 
     // Check if jquery is loaded
