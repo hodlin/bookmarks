00fc357ba7f116d460d1444cf385b954a3dea861
Dmytro Domin <mr.diomin@gmail.com>
2016-01-31 17:33:45 +0200

    get_absolute_url method to Image model added

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -27,3 +28,6 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
             super(Image, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
