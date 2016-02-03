0fb7d59e32ccf923c6b6c9677619f5140f128c28
Dmytro Domin <mr.diomin@gmail.com>
2016-02-03 17:05:02 +0200

    images list view

--- common/decorators.py
+++ common/decorators.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseBadRequest
 def ajax_required(f):
     def wrap(request, *args, **kwargs):
         if not request.is_ajax():
-            return HttpResponseBadRequest
+            return HttpResponseBadRequest()
         return f(request, *args, **kwargs)
     wrap.__doc__ = f.__doc__
     wrap.__name__ = f.__name__
--- images/views.py
+++ images/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -39,6 +40,33 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'images',
                                                         'image': image})
+
+
+@login_required
+def image_list(request):
+    images = Image.object.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images', 'images': images})
+
+
 
 
 @ajax_required
