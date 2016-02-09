22aaff1925ce30fa362c19c2a6846facd66f2e29
Dmytro Domin <mr.diomin@gmail.com>
2016-02-09 23:38:26 +0200

    image app config

--- images/signals.py
+++ images/signals.py
@@ -4,6 +4,6 @@ from .models import Image
 
 
 @receiver(m2m_changed, sender=Image.users_like.through)
-def users_like_changed(sender, instance, **kwargs)
+def users_like_changed(sender, instance, **kwargs):
     instance.total_likes = instance.users_like.count()
     instance.save()
