249a7c494ab9487fd8f8974b37f929424f334920
Dmytro Domin <mr.diomin@gmail.com>
2016-02-09 09:53:45 +0200

    user_follow view added

--- account/urls.py
+++ account/urls.py
@@ -54,6 +54,6 @@ urlpatterns = [
 
     url(r'^users/(?P<username>[-\w]+)/$', views.user_detail, name='user_detail'),
 
-
+    url(r'^users/follow/$', views.user_follow, name='user_follow'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 
 # Create your views here.
 
@@ -100,3 +102,24 @@ def user_detail(request, username):
                   'account/user/detail.html',
                   {'section': 'people',
                    'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'ko'})
+    return JsonResponse({'status': 'ko'})
