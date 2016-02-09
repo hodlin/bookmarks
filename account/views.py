981fc53f77f057bd074d593bf8877a400857b433
Dmytro Domin <mr.diomin@gmail.com>
2016-02-09 15:55:02 +0200

    adding actions to the activity stream

--- account/views.py
+++ account/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from actions.utils import create_action
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 
@@ -49,6 +50,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             profile = Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -117,6 +119,7 @@ def user_follow(request):
                 Contact.objects.get_or_create(
                                 user_from=request.user,
                                 user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
