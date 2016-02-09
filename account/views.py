3040b0704a557682cfb702b23bd5ebaa1013ca12
Dmytro Domin <mr.diomin@gmail.com>
2016-02-09 16:06:34 +0200

    select_related added to actions QuerySet

--- account/views.py
+++ account/views.py
@@ -42,7 +42,8 @@ def dashboard(request):
 
     if following_ids:
         # If user is following others, retrieve only their actions
-        actions = actions.filter(user_id__in=following_ids)
+        actions = actions.filter(user_id__in=following_ids)\
+                        .select_related('user', 'user__profile')
     actions = actions[:10]
 
     return render(request, 'account/dashboard.html', {'section': 'dashboard',
